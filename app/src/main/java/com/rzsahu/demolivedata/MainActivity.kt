798809a2cc8bf0a49bf404f5ab3d666e72a40166
rajesh <rajeshsahuise@gmail.com>
2023-07-30 12:38:20 +0530

    Replacing findViewById with data binding 1. Accessing the layout widgets through data binding

--- app/src/main/java/com/rzsahu/demolivedata/MainActivity.kt
+++ app/src/main/java/com/rzsahu/demolivedata/MainActivity.kt
@@ -4,27 +4,26 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.rzsahu.demolivedata.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
     private lateinit var mainViewModel: MainViewModel
-    private val infoTextView : TextView
-        get() = findViewById(R.id.information)
-    private val updateButton : Button
-        get() = findViewById(R.id.update)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         mainViewModel = ViewModelProvider(this)[MainViewModel::class.java]
         mainViewModel.getInfoLiveData().observe(this, Observer {
-            infoTextView.text = it
+            binding.information.text = it
         })
 
-        updateButton.setOnClickListener {
+        binding.update.setOnClickListener {
             mainViewModel.setInformation("Hi! I am updated on click")
         }
     }
